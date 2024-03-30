5b148758a22014d9d7f35db61ef91c3963ff829f
Luis Cortorreal <cort0038@algonquinlive.com>
2024-03-30 12:40:29 -0400

    changed project files structure

---
+++ app/components/Masthead.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+
+const Masthead = () => {
+	return (
+		<div>
+			<h1 className="mt-5 text-4xl md:text-5xl font-extrabold text-black  text-center">
+				Discover
+				<br />
+				<span className="blue_gradient text-center"> The Perfect Food &amp; Movie for Today</span>
+			</h1>
+			<p className="mt-2 text-lg text-gray-600 text-center sm:text-xl">
+				Get recommendations for movies to watch and delicious recipes to cook according to today&apos;s weather!
+			</p>
+		</div>
+	)
+}
+
+export default Masthead
