d1a102e8b2faa9019ec964af61a7ca1b51564d35
Luis Cortorreal <cort0038@algonquinlive.com>
2024-03-30 13:30:16 -0400

    maybe fixed "no response" error from API when deploying

--- app/api/weather/route.js
+++ app/api/weather/route.js
@@ -5,20 +5,21 @@ export async function GET(city, country) {
 	let url = `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${apiKey}&units=metric`
 
 	try {
-		const response = await axios.get(url)
+		const response = await new axios.get(url)
 
 		let data = response.data
 		if (data.length === 0) {
 			let message = "No location found"
 			// error(message)
 			console.warn(message)
+			return message
 		} else {
 			console.log(`${data.name}`, data)
 			return data
 		}
 	} catch {
 		let message = "Error fetching location"
-		// error(message)
 		console.warn(message)
+		return message
 	}
 }
