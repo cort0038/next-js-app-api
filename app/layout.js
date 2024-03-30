972f627b9e92d40f144573c94c7c1b4ec8149533
Luis Cortorreal <cort0038@algonquinlive.com>
2024-03-30 13:05:07 -0400

    added new favicon

--- app/layout.js
+++ app/layout.js
@@ -9,6 +9,9 @@ export const metadata = {
 export default function RootLayout({children}) {
 	return (
 		<html lang="en">
+			<head>
+				<link rel="icon" href="./favicon.ico" sizes="any"/>
+			</head>
 			<body>
 				<div className="main">
 					<div className="gradient" />
