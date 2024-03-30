34c4ff91284361ced8f76fe3269a53da356febf7
Luis Cortorreal <cort0038@algonquinlive.com>
2024-03-30 12:37:11 -0400

    Weather API Get Method Added

--- app/api/weather/route.js
+++ app/api/weather/route.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import axios from "axios"
 
-function route() {
-  return (
-    <div>
-      
-    </div>
-  )
+export async function GET(city, country) {
+	let apiKey = process.env.NEXT_PUBLIC_WEATHER_API_KEY
+	let url = `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${apiKey}&units=metric`
+
+	try {
+		const response = await axios.get(url)
+
+		if (response.data.length === 0) {
+			let message = "No location found"
+			// error(message)
+			console.warn(message)
+		} else {
+			let location = response.data
+			console.log(`${location.name}`, location)
+			return location
+		}
+	} catch {
+		let message = "Error fetching location"
+		// error(message)
+		console.warn(message)
+	}
 }
-
-export default route
