8e81293ff80918f02bcc1611fb8d3450d2ab240b
Luis Cortorreal <cort0038@algonquinlive.com>
2024-03-30 13:08:00 -0400

    converted to "use client"

--- app/page.js
+++ app/page.js
@@ -1,11 +1,21 @@
-import Masthead from "./components/masthead/Masthead"
-import SearchBar from "./components/searchBar/SearchBar"
+"use client"
+import Masthead from "@Components/Masthead"
+import SearchBar from "@Components/SearchBar"
+import WeatherCard from "@Components/WeatherCard"
+import {useState} from "react"
 
 export default function Home() {
+	const [data, setData] = useState({})
+
+	const handleSearch = data => {
+		setData(data)
+	}
+
 	return (
 		<div>
 			<Masthead />
-			<SearchBar />
+			<SearchBar onSearch={handleSearch} />
+			<WeatherCard data={data} />
 		</div>
 	)
 }
