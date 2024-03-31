31d2190245f33dee250a4190c3e4934cd71ac548
Luis Cortorreal <cort0038@algonquinlive.com>
2024-03-30 23:18:44 -0400

    created api movie routers

--- app/api/movies/route.js
+++ app/api/movies/route.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import {NextRequest, NextResponse} from "next/server"
 
-function route() {
-  return (
-    <div>
-      
-    </div>
-  )
+export async function GET(request) {
+	const params = new URL(request.url).searchParams
+	const weather = params.get("weather")
+
+	let url =
+		"https://api.themoviedb.org/3/search/movie?query=" +
+		weather +
+		"&include_adult=false&language=en-US&page=1&api_key=" +
+		"a948ce29db1844d126091636b22b38a6"
+
+	const res = await fetch(url)
+	let data = await res.json()
+	return NextResponse.json(data)
 }
-
-export default route
