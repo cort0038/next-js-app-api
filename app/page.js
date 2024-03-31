2e676d9f26295b2ba77783b087946aef507bc633
Luis Cortorreal <cort0038@algonquinlive.com>
2024-03-30 23:20:06 -0400

    removed unused components

--- app/page.js
+++ app/page.js
@@ -1,21 +1,11 @@
-"use client"
 import Masthead from "@Components/Masthead"
 import SearchBar from "@Components/SearchBar"
-import WeatherCard from "@Components/WeatherCard"
-import {useState} from "react"
 
 export default function Home() {
-	const [data, setData] = useState({})
-
-	const handleSearch = data => {
-		setData(data)
-	}
-
 	return (
 		<div>
 			<Masthead />
-			<SearchBar onSearch={handleSearch} />
-			<WeatherCard data={data} />
+			<SearchBar />
 		</div>
 	)
 }
