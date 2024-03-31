a83130536a43a1c16bc6e41a4cec89768b289a35
Luis Cortorreal <cort0038@algonquinlive.com>
2024-03-30 23:22:53 -0400

    no important changes

--- app/components/RecipeCard.jsx
+++ app/components/RecipeCard.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import PropTypes from "prop-types"
 
-const RecipeCard = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+export default function RecipeCard({weather}) {
+	if (weather) {
+		console.log(weather.conditions)
+		console.log(weather)
+	}
+
+	return (
+		<div className="rounded-lg text-black bg-slate-300 pr-12 pl-12 pt-6 pb-6 text-center cursor-pointer w-fit">
+			<h3>Today&apos;s Perfect Recipe</h3>
+		</div>
+	)
 }
 
-export default RecipeCard
+RecipeCard.propTypes = {
+	weather: PropTypes.object,
+}
