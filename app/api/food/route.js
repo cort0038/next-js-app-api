f0910dc9af7e5e43cbb596ec06d2026575874bf5
Luis Cortorreal <cort0038@algonquinlive.com>
2024-03-31 12:30:31 -0400

    added api food

--- app/api/food/route.js
+++ app/api/food/route.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import {NextRequest, NextResponse} from "next/server"
 
-function route() {
-  return (
-    <div>
-      
-    </div>
-  )
+export async function GET(request) {
+	const params = new URL(request.url).searchParams
+	const weather = params.get("weather")
+
+	let url =
+		"https://api.spoonacular.com/recipes/complexSearch?query=" +
+		weather +
+		"&apiKey=" +
+		"3aa5ed4456b149bf814db448045e27f4"
+
+	const res = await fetch(url)
+	let data = await res.json()
+	return NextResponse.json(data)
 }
 
-export default route
