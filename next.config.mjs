3dceba0117b4f5b9d136d38acb5d5f210c14a727
Luis Cortorreal <cort0038@algonquinlive.com>
2024-03-31 14:42:34 -0400

    added openweathermap.org domain

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
 	reactStrictMode: true,
 	images: {
-		domains: ["localhost", "image.tmdb.org", "img.spoonacular.com"]
+		domains: ["localhost", "image.tmdb.org", "img.spoonacular.com", "openweathermap.org"]
 	}
 }
 
