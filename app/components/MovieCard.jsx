0d5f7825ee70fe2c5268180891fb31f6b678571b
Luis Cortorreal <cort0038@algonquinlive.com>
2024-03-30 23:22:25 -0400

    added navigation routes

--- app/components/MovieCard.jsx
+++ app/components/MovieCard.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import {useRouter} from "next/navigation"
+import PropTypes from "prop-types"
 
-function MovieCard() {
-  return (
-    <div>
-      
-    </div>
-  )
+export default function MovieCard({data, input}) {
+	const router = useRouter()
+
+	if (!data) {
+		return null
+	}
+	let conditions = data.weather[0].main
+	let url = "/" + encodeURIComponent(input) + "/movie?w=" + conditions
+
+	const handleSubmit = async ev => {
+		ev.preventDefault()
+		router.push(url)
+	}
+
+	return (
+		<div
+			className="rounded-lg text-black bg-slate-300 pr-12 pl-12 pt-6 pb-6 text-center cursor-pointer w-fit"
+			onClick={handleSubmit}>
+			<h3>Today&apos;s Perfect Movie</h3>
+		</div>
+	)
 }
 
-export default MovieCard
+MovieCard.propTypes = {
+	data: PropTypes.object,
+	input: PropTypes.string,
+}
