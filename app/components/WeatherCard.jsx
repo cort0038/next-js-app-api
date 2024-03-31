b911c58cdd0157ac9828e9c4ce4e2d04431a4a7e
Luis Cortorreal <cort0038@algonquinlive.com>
2024-03-30 23:20:56 -0400

    renamed variables

--- app/components/WeatherCard.jsx
+++ app/components/WeatherCard.jsx
@@ -2,15 +2,17 @@ import PropTypes from "prop-types"
 
 const WeatherCard = ({data}) => {
 	return (
-		<div className="flex items-center justify-center">
-			<div className="rounded-lg bg-cyan-200 text-black pr-12 pl-12 pt-6 pb-6 text-center cursor-pointer  w-fit">
-				<h2>{data.city}</h2>
-				<p>{data.country}</p>
-				<p>{data.temperature}</p>
-				<p>{data.weather}</p>
-				<p>{data.windspeed}</p>
+		data && (
+			<div className="flex items-center justify-center">
+				<div className="rounded-lg bg-cyan-200 text-black pr-12 pl-12 pt-6 pb-6 text-center cursor-pointer  w-fit">
+					<h2>{data.name}</h2>
+					<p>{data.sys.country}</p>
+					<p>{data.weather[0].main}</p>
+					<p>{data.main.temp}</p>
+					<p>{data.wind.speed}</p>
+				</div>
 			</div>
-		</div>
+		)
 	)
 }
 
