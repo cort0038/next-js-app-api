d738c52f803eda3a6e53fe3fe828516ba0dbfefa
Luis Cortorreal <cort0038@algonquinlive.com>
2024-04-01 00:11:57 -0400

    minimum changes

--- app/api/food/route.js
+++ app/api/food/route.js
@@ -4,11 +4,7 @@ export async function GET(request) {
 	const params = new URL(request.url).searchParams
 	const weather = params.get("weather")
 
-	let url =
-		"https://api.spoonacular.com/recipes/complexSearch?query=" +
-		weather +
-		"&apiKey=" +
-		"3aa5ed4456b149bf814db448045e27f4"
+	let url = `https://api.spoonacular.com/recipes/complexSearch?query=${weather}&apiKey=3aa5ed4456b149bf814db448045e27f4`
 
 	const res = await fetch(url)
 
--- app/api/movies/route.js
+++ app/api/movies/route.js
@@ -4,11 +4,7 @@ export async function GET(request) {
 	const params = new URL(request.url).searchParams
 	const weather = params.get("weather")
 
-	let url =
-		"https://api.themoviedb.org/3/search/movie?query=" +
-		weather +
-		"&include_adult=false&language=en-US&page=1&api_key=" +
-		"a948ce29db1844d126091636b22b38a6"
+	let url = `https://api.themoviedb.org/3/search/movie?query=${weather}&include_adult=false&language=en-US&page=1&api_key=a948ce29db1844d126091636b22b38a6`
 
 	const res = await fetch(url)
 
