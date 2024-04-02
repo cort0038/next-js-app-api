5ad489bbc700e8aebb430c01bec9384b6b700481
Luis Cortorreal <cort0038@algonquinlive.com>
2024-04-01 22:58:56 -0400

    added new image domain

--- next.config.mjs
+++ next.config.mjs
@@ -29,6 +29,12 @@ const nextConfig = {
 				hostname: "via.placeholder.com",
 				port: "",
 				pathname: "/**"
+			},
+			{
+				protocol: "https",
+				hostname: "edamam-product-images.s3.amazonaws.com",
+				port: "",
+				pathname: "/**"
 			}
 		]
 	}
